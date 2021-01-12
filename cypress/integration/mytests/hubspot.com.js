c461d8a2b74bc1895724780999359ee67aac33f6
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-12 15:13:29 +0530

    Added intellisense or autosuggestion using jsconfig.json file and also added hubspot for uncaught expections handling

---
+++ cypress/integration/mytests/hubspot.com.js
@@ -0,0 +1,10 @@
+describe('hubspot page feature test',()=>{
+
+it('handle hubspot login test',()=> {
+    cy.visit('https://app.hubspot.com/login',{failOnStatusCode: false})
+   /* cy.get('#username').type('mail2ramyaak@gmail.com')
+    cy.get('#password').type('Test@1234567890')
+    cy.get('#loginBtn').click()*/
+    
+})
+})
