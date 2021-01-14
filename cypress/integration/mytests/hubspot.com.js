ecfe20624ad7bc579a5c663df8a113dfadc27f60
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-14 20:17:30 +0530

    adding new code

--- cypress/integration/mytests/hubspot.com.js
+++ cypress/integration/mytests/hubspot.com.js
@@ -1,9 +1,9 @@
 describe('hubspot page feature test',()=>{
 
 it('handle hubspot login test',()=> {
-    cy.visit('https://app.hubspot.com/login',{failOnStatusCode: false})
-   /* cy.get('#username').type('mail2ramyaak@gmail.com')
-    cy.get('#password').type('Test@1234567890')
+    //cy.visit('https://app.hubspot.com/login',{failOnStatusCode: false})
+   /* cy.get('#username',{timeout:30000}).type('mail2ramyaak@gmail.com')
+    cy.get('#password',{timeout:30000}).type('Test@1234567890')
     cy.get('#loginBtn').click()*/
     
 })
