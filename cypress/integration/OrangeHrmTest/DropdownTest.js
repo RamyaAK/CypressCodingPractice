8f4e1448b71c2897e8aac5dac4fc32f3c9487c56
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-16 22:13:28 +0530

    Added test to show how to use select for dropdowns using cypress

---
+++ cypress/integration/OrangeHrmTest/DropdownTest.js
@@ -0,0 +1,8 @@
+describe('Test the Dropdown using the select tag in  cypress', ()=>{
+
+    it("Select to test and get a perticular element from the dropdown menue",()=>{
+        cy.visit('https://www.orangehrm.com/orangehrm-30-day-trial/')
+        cy.get('#Form_submitForm_Country').select('Costa Rica')
+        
+    })
+})
