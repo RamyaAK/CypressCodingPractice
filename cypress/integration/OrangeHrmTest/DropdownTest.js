8d85783a074fb70b1705cfec8d8d566429ff984f
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-18 22:52:29 +0530

    Added test to demo handling dropdown with get compare and click

--- cypress/integration/OrangeHrmTest/DropdownTest.js
+++ cypress/integration/OrangeHrmTest/DropdownTest.js
@@ -1,9 +1,17 @@
 describe('Test the Dropdown using the select tag in  cypress', ()=>{
 
-    it("Select to test and get a perticular element from the dropdown menue",()=>{
+    it.skip("Select to test and get a perticular element from the dropdown menue",()=>{
         cy.visit('https://www.orangehrm.com/orangehrm-30-day-trial/')
         cy.get('#Form_submitForm_Country').select('Costa Rica')
         cy.should('have.value','Costa Rica');
         
+    })
+
+
+    it('Google search test dropdowns',()=>{
+        cy.visit('https://www.google.com')
+        cy.get('.gLFyf').type("cypress tests")
+
+        cy.get('div.jKWzZXdEJWi__suggestions-inner-container').contains('cypress tests').click()
     })
 })
