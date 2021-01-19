09cd5e2ce57141549eb77fa9a571cad75f7b5c30
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-19 11:44:35 +0530

    Added test to demo handling mouseOver event

--- cypress/integration/mytests/mouseoverDemoTest.js
+++ cypress/integration/mytests/mouseoverDemoTest.js
@@ -1,8 +1,19 @@
 describe('Mouseover functionality test in cypress',()=>{
-    it('Testing how to mouseover in cypress',()=>{
+    it.skip('Spicejet signUp test',()=>{
         cy.visit('https://www.spicejet.com')
         cy.contains('Login / Signup').trigger('mouseover')
         cy.contains('SpiceClub Members').trigger('mouseover')
         cy.contains('Sign up').click()
+    })
+
+    it.skip('Add to cart Test',()=>{
+        cy.visit('http://automationpractice.com/index.php')
+        cy.get('.ajax_add_to_cart_button').first().click()
+        cy.get('.cross').click()
+        cy.wait(3000)
+        cy.get('.cart_block').should('be.hidden').invoke('show')
+        cy.get('#button_order_cart').click()
+        cy.url().should('include','controller=order')
+
     })
 })
