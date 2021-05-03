11d4e9b409985588f49a676e1b6bfda847bd9df0
RamyaK <ext_rakulkarni@falabella.cl>
2021-05-03 12:41:49 +0530

    Cypress practice continued

--- cypress/integration/worth2Read.comTests/titleNameTest.js
+++ cypress/integration/worth2Read.comTests/titleNameTest.js
@@ -1,11 +1,21 @@
 describe('elements of Worth2Read.com Test',()=>{
+   it('Verify if the Heading Name is Worth2Read.com',()=>{
+      cy.visit("");
+      cy.contains('Worth2Read');
+      cy.contains('Worth2Read').click();
+      cy.url().should('include','ramyakulkarni');
+      cy.contains('Continue reading').click();
+      /* ==== Generated with Cypress Studio ==== */
+      cy.get('#search-2 > .search-form > label > .search-field').click();
+      cy.get('#search-2 > .search-form > label > .search-field').type('Indian bengak{backspace}l');
+      cy.get('#search-2 > .search-form > .search-submit').click();
+      /* ==== End Cypress Studio ==== */
+   });
 
-    it('Verify if the Heading Name is Worth2Read.com',()=>{
-       cy.visit("");
-       cy.contains('Worth2Read');
-       cy.contains('Worth2Read').click();
-       cy.url().should('include','ramyakulkarni');
-       cy.contains('Continue reading').click();
-    });
-
+   /* === Test Created with Cypress Studio === */
+   it('New Title Test', function() {
+      /* ==== Generated with Cypress Studio ==== */
+      cy.visit('https://ramyakulkarni.wordpress.com/');
+      /* ==== End Cypress Studio ==== */
+   });
 });
